032847f0503f31793d2fa360c0c1790fe99376a6
KiraleDev <dev@kirale.com>
2018-12-04 09:54:06 +0100

    Not using python-iptables, forcing aiocoap version

--- setup.py
+++ setup.py
@@ -53,8 +53,8 @@ setup(
     include_package_data=True,
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'kitools', 'bash', 'pyroute2', 'python-iptables', 'aiocoap',
-        'pycryptodomex', 'zeroconf'
+        'kitools', 'bash', 'pyroute2', 'aiocoap==0.4a1', 'pycryptodomex',
+        'zeroconf'
     ],
     entry_points={'console_scripts': ['kibra = kibra.__main__:main']},
 )
