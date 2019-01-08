fbc3c350fa8c0af91a020050fa44127525093968
KiraleDev <dev@kirale.com>
2019-01-08 10:54:14 +0100

    coapclient debug logging

--- kibra/coapclient.py
+++ kibra/coapclient.py
@@ -1,15 +1,16 @@
-import logging
 import asyncio
+import logging
 
 import aiocoap
 from aiocoap.numbers.codes import Code
+from kibra.tlv import ThreadTLV
 
 
 class CoapClient():
     '''Perform CoAP petitions to the Thread Diagnostics port'''
 
     def __init__(self):
-        self.protocol = None
+        self.context = None
 
     async def con_request(self, addr, port, path, payload=''):
         return await self.request(addr, port, path, aiocoap.CON, payload)
@@ -19,26 +20,29 @@ class CoapClient():
 
     async def request(self, addr, port, path, mtype, payload=''):
         '''Client request'''
-        if self.protocol is None:
-            self.protocol = await aiocoap.Context.create_client_context()
+        if self.context is None:
+            self.context = await aiocoap.Context.create_client_context()
         req = aiocoap.Message(code=Code.POST, mtype=mtype, payload=payload)
         uri = 'coap://[%s]:%u%s' % (addr, port, path)
         req.set_request_uri(uri=uri, set_uri_host=False)
+        logging.debug('tx: %s %s' % (uri, ThreadTLV.sub_tlvs_str(payload)))
         try:
             # Workaround for not waiting a response to a non-confirmable request
             if mtype == aiocoap.NON:
                 try:
                     await asyncio.wait_for(
-                        self.protocol.request(req).response, timeout=0.01)
+                        self.context.request(req).response, timeout=0.01)
                 except asyncio.TimeoutError:
                     return
             else:
-                response = await self.protocol.request(req).response
+                response = await self.context.request(req).response
         except:
-            logging.debug('No response from %s', addr)
+            logging.warn('No response from %s', addr)
         else:
-            logging.debug('%s responded with %s.', addr, response.code)
+            logging.debug(
+                'rx: %s %s %s' % (addr, response.code,
+                                  ThreadTLV.sub_tlvs_str(response.payload)))
             return response.payload
 
     def stop(self):
-        self.protocol.shutdown()
+        self.context.shutdown()
