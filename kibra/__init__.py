10142e9b5cbf718a3342c7972e7a33ec19a93935
KiraleDev <dev@kirale.com>
2019-03-15 14:50:52 +0100

    Updated required KiNOS version

--- kibra/__init__.py
+++ kibra/__init__.py
@@ -3,5 +3,4 @@
 __vendor__ = 'Kirale'
 __model__ = 'KTBRN1'
 __version__ = '1.2.5'
-#__kinosver__ = 'KiNOS Golden v1.3.7156.65667'
-__kinosver__ = 'KiNOS Golden v1.3.'  # Wildcard for developing
+__kinosver__ = 'KiNOS Golden v1.3.7175.66245'
