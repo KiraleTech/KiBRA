08bcba625edbb491d4b325e138c69d45b17aca02
KiraleDev <dev@kirale.com>
2019-10-16 11:34:05 +0200

    Fix KiNOS version

--- kibra/__init__.py
+++ kibra/__init__.py
@@ -3,7 +3,7 @@
 __vendor__ = 'Kirale'
 __model__ = 'KTBRN1'
 __version__ = '1.4.6'
-__kinosver__ = 'KiNOS v1.3.7393.6183'
+__kinosver__ = 'KiNOS v1.3.7393.61873'
 
 # Used to enable or disable code if KiBRA is to be used in a Thread Harness environment
 __harness__ = 'Golden' in __kinosver__
